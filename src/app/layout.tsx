bf99d338528ebd0140c2e8019520ff5cbaba517b
Radek Skrabucha <rskrabucha13@gmail.com>
2023-06-23 15:13:41 +0200

    feat(icons): added new way of dealing with icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,18 @@ export const metadata = {
 
 const RootLayout: React.FC<React.PropsWithChildren> = ({ children }) => (
   <html lang="en">
-    <link
-      rel="icon"
-      type="image/svg+xml"
-      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥑</text></svg>"
-    />
+    <head>
+      <link
+        rel="icon"
+        type="image/svg+xml"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥑</text></svg>"
+      />
+      <link
+        rel="preload"
+        as="image/svg+xml"
+        href="sprite.svg"
+      />
+    </head>
     <body
       className={`${MainFont.variable} ${SecondaryFont.variable} relative flex min-h-[100dvh] flex-col items-center overflow-x-hidden bg-slate-200 font-secondary`}
     >
--- src/utils/icons.tsx
+++
@@ -1,5 +0,0 @@
-export const iconWithClassName =
-  (IconComponent: React.ElementType) =>
-  // eslint-disable-next-line react/display-name
-  ({ className }: Pick<React.ComponentProps<'svg'>, 'className'>) =>
-    <IconComponent className={className} />
