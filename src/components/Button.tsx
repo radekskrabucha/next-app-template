1a34836192a865e854a2daa990d9fe37755eb969
Radek Skrabucha <rskrabucha13@gmail.com>
2023-06-28 11:20:48 +0200

    feat(shadcn): added shadcn and shadcn button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,48 +1,74 @@
 import { Slot } from '@radix-ui/react-slot'
-import { cva, cx, type VariantProps } from 'class-variance-authority'
+import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react'
+import { cn } from '@/utils'
 
-type ButtonProps = {
+export type ButtonProps = {
   asChild?: boolean
 } & Pick<
   React.ComponentProps<'button'>,
   'children' | 'className' | 'onClick' | 'disabled' | 'type'
 > &
-  VariantProps<typeof buttonStyles>
+  VariantProps<typeof buttonVariants>
 
-export const Button: React.FunctionComponent<ButtonProps> = ({
-  variant,
-  className,
-  children,
-  type = 'button',
-  disabled,
-  onClick,
-  asChild
-}) => {
-  const Comp = asChild ? Slot : 'button'
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      type = 'button',
+      disabled,
+      onClick,
+      children
+    },
+    ref
+  ) => {
+    const Comp = asChild ? Slot : 'button'
 
-  return (
-    <Comp
-      className={cx('', buttonStyles({ variant }), className)}
-      type={type}
-      disabled={disabled}
-      onClick={onClick}
-    >
-      {children}
-    </Comp>
-  )
-}
-export const buttonStyles = cva(
-  'inline-flex py-2 px-4 min-w-[180px] items-center justify-center transition-color duration-150 rounded-md disabled:cursor-not-allowed',
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        type={type}
+        disabled={disabled}
+        onClick={onClick}
+      >
+        {children}
+      </Comp>
+    )
+  }
+)
+Button.displayName = 'Button'
+
+export const buttonVariants = cva(
+  'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-800',
   {
     variants: {
       variant: {
-        outlined:
-          'text-blue-500 bg-transparent border-blue-500 border-2 disabled:border-blue-300 disabled:text-blue-300 hover:border-blue-400 hover:bg-blue-100 disabled:bg-transparent hover:text-blue-400',
-        filled: 'bg-blue-500 text-white disabled:bg-blue-300 hover:bg-blue-400'
+        default:
+          'bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90',
+        destructive:
+          'bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-red-50 dark:hover:bg-red-900/90',
+        outline:
+          'border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50',
+        secondary:
+          'bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80',
+        ghost:
+          'hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50',
+        link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50'
+      },
+      size: {
+        default: 'h-10 px-4 py-2',
+        sm: 'h-9 rounded-md px-3',
+        lg: 'h-11 rounded-md px-8',
+        icon: 'h-10 w-10'
       }
     },
     defaultVariants: {
-      variant: 'filled'
+      variant: 'default',
+      size: 'default'
     }
   }
 )
